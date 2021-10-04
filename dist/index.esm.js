869b49c4ef33aab409bd9ac85f9bfe6a54e05860
Poly-Vertex <polygon.vertex.farm@gmail.com>
2021-10-04 14:16:26 -0400

    remove binance chain wallet

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2040,7 +2040,7 @@ var Icon$16 = function (props) {
 };
 
 var Icon$17 = function (props) {
-    return (React.createElement(Svg, { xmlns: "http://www.w3.org/2000/svg", x: "0", y: "0", enableBackground: "new 0 0 122.88 122.34", version: "1.1", viewBox: "0 0 122.88 122.34", xmlSpace: "preserve" },
+    return (React.createElement(Svg, __assign({ viewBox: "-60 -30 600 600" }, props),
         React.createElement("path", { d: "M58.61 28.13c-1.3 0-2.35-1.05-2.35-2.35s1.05-2.35 2.35-2.35h16.22c1.3 0 2.35 1.05 2.35 2.35s-1.05 2.35-2.35 2.35H58.61zm26.89-2.97h22.19c.97 0 1.8.59 2.16 1.42l12.57 24.27c.2.38.28.8.26 1.2l.21 67.94c0 1.29-1.05 2.34-2.34 2.34v.01H2.35c-1.3 0-2.35-1.05-2.35-2.35V51.64l.01-.04v-.02l.01-.04.01-.04v-.02l.01-.04.01-.02V51.36l.01-.02.01-.02v-.02l.01-.02.01-.02v-.02l.01-.02v-.01l.01-.03.01-.02.01-.02.01-.02.01-.02.01-.02.01-.02.01-.02L.2 51l.02-.04.01-.02.01-.01v-.01l.01-.02.01-.02.01-.02.01-.02.02-.04.02-.03.01-.02.01-.02.01-.02 12.78-24.3a2.33 2.33 0 012.07-1.25v-.01h21.7V2.91c0-.8.33-1.53.85-2.06.53-.52 1.26-.85 2.06-.85h42.78c.8 0 1.53.33 2.06.85.53.53.85 1.25.85 2.06v22.25zm-48.6 4.69H16.61L6.23 49.6H116.5l-10.23-19.75H85.5v7.35h5.17c1.3 0 2.35 1.05 2.35 2.35s-1.05 2.35-2.35 2.35H32.95c-1.3 0-2.35-1.05-2.35-2.35s1.05-2.35 2.35-2.35h3.96v-7.35h-.01zM4.69 54.3v63.35h113.5L118 54.3H4.69zM80.81 4.69H41.59V37.2h39.22V4.69zM47.58 17.11c-1.3 0-2.35-1.05-2.35-2.35s1.05-2.35 2.35-2.35h27.24c1.3 0 2.35 1.05 2.35 2.35s-1.05 2.35-2.35 2.35H47.58z" })));
 };
 
@@ -2533,21 +2533,6 @@ var Icon$1c = function (props) {
         React.createElement("path", { d: "M29.6927 35.4245C39.8036 25.5252 56.1965 25.5252 66.3074 35.4245L67.5242 36.6159C68.0298 37.1109 68.0298 37.9134 67.5242 38.4084L63.3616 42.4839C63.1088 42.7314 62.699 42.7314 62.4462 42.4839L60.7717 40.8444C53.7181 33.9384 42.282 33.9384 35.2284 40.8444L33.4351 42.6002C33.1823 42.8477 32.7725 42.8477 32.5197 42.6002L28.3571 38.5247C27.8515 38.0297 27.8515 37.2272 28.3571 36.7322L29.6927 35.4245ZM74.9161 43.8532L78.6208 47.4805C79.1264 47.9755 79.1264 48.778 78.6208 49.2729L61.9159 65.6288C61.4103 66.1237 60.5907 66.1237 60.0851 65.6288C60.0851 65.6288 60.0851 65.6288 60.0851 65.6288L48.229 54.0206C48.1026 53.8968 47.8977 53.8968 47.7713 54.0206C47.7713 54.0206 47.7713 54.0206 47.7713 54.0206L35.9153 65.6288C35.4098 66.1237 34.5902 66.1237 34.0846 65.6288C34.0846 65.6288 34.0846 65.6288 34.0846 65.6288L17.3792 49.2727C16.8736 48.7778 16.8736 47.9753 17.3792 47.4803L21.0839 43.853C21.5895 43.3581 22.4091 43.3581 22.9146 43.853L34.771 55.4614C34.8974 55.5851 35.1023 55.5851 35.2287 55.4614C35.2287 55.4614 35.2287 55.4614 35.2287 55.4614L47.0844 43.853C47.59 43.358 48.4096 43.358 48.9152 43.853C48.9152 43.853 48.9152 43.853 48.9152 43.853L60.7715 55.4614C60.8979 55.5851 61.1028 55.5851 61.2292 55.4614L73.0854 43.8532C73.5909 43.3583 74.4105 43.3583 74.9161 43.8532Z", fill: "white" })));
 };
 
-var Icon$1d = function (props) {
-    return (React.createElement(Svg, __assign({ viewBox: "0 0 32 32" }, props),
-        React.createElement("path", { d: "M24 0H8C3.58172 0 0 3.58172 0 8V24C0 28.4183 3.58172 32 8 32H24C28.4183 32 32 28.4183 32 24V8C32 3.58172 28.4183 0 24 0Z", fill: "#1E2026" }),
-        React.createElement("path", { d: "M16.2857 4L9.97035 7.6761L12.2922 9.03415L16.2857 6.7161L20.2792 9.03415L22.6011 7.6761L16.2857 4Z", fill: "#F0B90B" }),
-        React.createElement("path", { d: "M20.2792 10.9541L22.6011 12.3122V15.0283L18.6075 17.3463V21.9824L16.2857 23.3405L13.9639 21.9824V17.3463L9.97035 15.0283V12.3122L12.2922 10.9541L16.2857 13.2722L20.2792 10.9541Z", fill: "#F0B90B" }),
-        React.createElement("path", { d: "M22.6011 16.9483V19.6644L20.2792 21.0224V18.3063L22.6011 16.9483Z", fill: "#F0B90B" }),
-        React.createElement("path", { d: "M20.2561 22.9424L24.2496 20.6244V15.9883L26.5714 14.6302V21.9824L20.2561 25.6585V22.9424Z", fill: "#F0B90B" }),
-        React.createElement("path", { d: "M24.2496 11.3522L21.9278 9.99414L24.2496 8.63609L26.5714 9.99414V12.7102L24.2496 14.0683V11.3522Z", fill: "#F0B90B" }),
-        React.createElement("path", { d: "M13.9639 26.642V23.9259L16.2857 25.2839L18.6075 23.9259V26.642L16.2857 28L13.9639 26.642Z", fill: "#F0B90B" }),
-        React.createElement("path", { d: "M12.2922 21.0224L9.97035 19.6644V16.9483L12.2922 18.3063V21.0224Z", fill: "#F0B90B" }),
-        React.createElement("path", { d: "M16.2857 11.3522L13.9639 9.99414L16.2857 8.63609L18.6075 9.99414L16.2857 11.3522Z", fill: "#F0B90B" }),
-        React.createElement("path", { d: "M10.6437 9.99414L8.32183 11.3522V14.0683L6 12.7102V9.99414L8.32183 8.63609L10.6437 9.99414Z", fill: "#F0B90B" }),
-        React.createElement("path", { d: "M6 14.6302L8.32183 15.9883V20.6244L12.3154 22.9424V25.6585L6 21.9824V14.6302Z", fill: "#F0B90B" })));
-};
-
 var connectors = [
     {
         title: "Metamask",
@@ -2573,11 +2558,6 @@ var connectors = [
         title: "WalletConnect",
         icon: Icon$1c,
         connectorId: "walletconnect",
-    },
-    {
-        title: "Binance Chain Wallet",
-        icon: Icon$1d,
-        connectorId: "bsc",
     },
 ];
 var localStorageKey = "accountStatus";
@@ -2666,7 +2646,7 @@ var UserBlock = function (_a) {
         } }, "Connect"))));
 };
 
-var Icon$1e = function (props) {
+var Icon$1d = function (props) {
     var theme = useTheme();
     var primaryColor = theme.isDark ? "#3C3742" : "#e9eaeb";
     var secondaryColor = theme.isDark ? "#666171" : "#bdc2c4";
@@ -2692,7 +2672,7 @@ var Avatar = function (_a) {
     var link = profile.username ? profileLink : noProfileLink;
     var isExternal = link.startsWith("http");
     var ariaLabel = "Link to profile";
-    var icon = image ? (React.createElement("img", { src: image, alt: "profile avatar", height: "32px", width: "32px" })) : (React.createElement(Icon$1e, { width: "32px", height: "32px" }));
+    var icon = image ? (React.createElement("img", { src: image, alt: "profile avatar", height: "32px", width: "32px" })) : (React.createElement(Icon$1d, { width: "32px", height: "32px" }));
     if (isExternal) {
         return (React.createElement(StyledAvatar, { title: username },
             React.createElement("a", { href: link, "aria-label": ariaLabel }, icon),
