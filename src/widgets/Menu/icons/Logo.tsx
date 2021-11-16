23b2071cf2cbb3c4a42db679715736c05091c689
Poly-Vertex <polygon.vertex.farm@gmail.com>
2021-11-16 12:33:08 -0500

    refactoring

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -13,7 +13,7 @@ const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
       <image
         width="205"
         height="26"
-        href={isDark ? "/images/egg/LogoTextNewDark.png" : "/images/egg/LogoTextNewWhite.png"}
+        href={isDark ? "/images/vert/LogoTextNewDark.png" : "/images/vert/LogoTextNewWhite.png"}
       />
     </Svg>
   );
